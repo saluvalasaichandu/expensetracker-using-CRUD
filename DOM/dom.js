ade58229cd37c0d70fc0d316089989cb641fce59
saluvalasaichandu <saluvalasaichand@gmail.com>
2022-11-14 00:15:54 +0530

    domgit

---
+++ DOM/dom.js
@@ -0,0 +1,16 @@
+//console.dir(document);
+console.log(document.URL);
+console.log(document.title);
+document.title="helloo";
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.forms);
+console.log(document.links);
+
+console.log(document.getElementById('header-title'));
+
+var headertitle=document.getElementById('header-title');
+
+headertitle.textContent="changed-newTitle";
