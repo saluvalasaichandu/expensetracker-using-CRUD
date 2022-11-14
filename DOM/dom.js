6ec593f8622d1bdefc453edb349e5b04d1c5a2aa
saluvalasaichandu <saluvalasaichand@gmail.com>
2022-11-14 20:43:04 +0530

    queryselec

--- DOM/dom.js
+++ DOM/dom.js
@@ -37,6 +37,43 @@ li[2].style.backgroundColor='green';
 for(var i=0;i<li.length;i++){
     li[i].style.fontWeight='bold';}
 
+//queryselector's
+
+
+ var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = "Hello World"
+
+ var submit = document.querySelector('input[type="submit"]');
+ submit.value = "SEND"
+
+ var item = document.querySelector('.list-group-item');
+ item.style.color = 'red';
+
+var secondItem = document.querySelector('.list-group-item');
+item.style.backgroundColor = 'green';
+
+//queryselector's...
+
+var secondItem = document.querySelectorAll('li:nth-child(2)');
+ item.style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+
+for(var i=0; i<odd.length; i++) 
+{
+ odd[i].style.backgroundColor = 'green';
+ 
+ }
+
+
+
+
+
+
 
 
 
