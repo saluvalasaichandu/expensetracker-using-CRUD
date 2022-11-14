4e0240d1ea89c14bf0ba28c01b36a792daa2da4b
saluvalasaichandu <saluvalasaichand@gmail.com>
2022-11-14 19:48:41 +0530

    dom classid

--- DOM/dom.js
+++ DOM/dom.js
@@ -14,3 +14,15 @@ console.log(document.getElementById('header-title'));
 var headertitle=document.getElementById('header-title');
 
 headertitle.textContent="changed-newTitle";
+
+
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[2].textContent='group3';
+items[2].style.backgroundColor='green';
+items[0].style.fontWeight='bold';
+items[1].style.fontWeight='bold';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
