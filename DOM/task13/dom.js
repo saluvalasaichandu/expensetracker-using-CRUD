9f1e5b3be76363abc4ae9e183b9c71a8cee1b408
saluvalasaichandu <saluvalasaichand@gmail.com>
2022-11-23 20:17:13 +0530

    project-Ajs

--- DOM/task13/dom.js
+++ DOM/task13/dom.js
@@ -1,85 +1,74 @@
-var namei=document.getElementById('name')
-var emaili=document.getElementById('email')
+var Form=document.getElementById('addform')
+var exp=document.getElementById('expence');
+var desc=document.getElementById('description');
+var cate=document.getElementById('category');
+var itemlist=document.getElementById('users')
+document.addEventListener("submit",addItem)
+localStorage.setItem('userexpense','expence')
+localStorage.setItem('userdescription','description')
+localStorage.setItem('usercategory','category')
 
-var itemList=document.querySelector('#users')
-var myForm=document.querySelector('#my-form');
-myForm.addEventListener('submit',onsubmit);
-
-
-function onsubmit(e){
+function addItem(e){
     e.preventDefault();
-    if(namei.value==='' || emaili.value===''){
+    if(exp.value==='' || desc.value==='' || cate.value===''){
         alert('please enter fields')
     }
     else{
-        if(localStorage.getItem(emaili.value)){
-            var it=document.querySelectorAll('.items');
-            for(var i=0;i<it.length;i++){
-                if(it[i].innerHTML.indexOf(emaili.value)!=-1){
-                    it[i].remove();
-                }
+        let Obj={
+            expence:exp.value,
+            description:desc.value,
+            category:cate.value
+        }
+        let Obj_serialized=JSON.stringify(Obj);
+localStorage.setItem(Obj.description,Obj_serialized);
+let Obj_deserialized=JSON.parse(localStorage.getItem("Obj"))
+
+var li=document.createElement('li');
+li.className='item';
+li.appendChild(document.createTextNode(localStorage.getItem(Obj.description)));
+
+var delbtn=document.createElement('button');
+delbtn.className='delete-btn';
+delbtn.appendChild(document.createTextNode('Delete'));
+li.appendChild(delbtn)
+
+var edibtn=document.createElement('button');
+edibtn.className='edit-btn';
+edibtn.appendChild(document.createTextNode('Edit'));
+li.appendChild(edibtn)
+
+itemlist.appendChild(li)
+    
+
+    document.addEventListener('click',deleteitem);
+
+    function deleteitem(e){
+        if(e.target==delbtn){
+            if(confirm('Are U sure?')){
+                var li=e.target.parentElement;
+                localStorage.removeItem(Obj.description);
+                itemlist.removeChild(li)
             }
         }
-let myObj={
-    name:namei.value,
-    email:emaili.value
-}
-let myobj_serialized=JSON.stringify(myObj);
-localStorage.setItem(myObj.email,myobj_serialized);
-let myobj_deserialized=JSON.parse(localStorage.getItem("myObj"))
-
-
-
-        
-var li=document.createElement('li');
-li.className='items';
-li.appendChild(document.createTextNode(localStorage.getItem(myObj.email)));
-
-
-
-
-var deletbtn=document.createElement('delete');
-deletbtn.className='btn';
-deletbtn.appendChild(document.createTextNode('delete'));
-li.appendChild(deletbtn);
-
-var editbtn=document.createElement('edit');
-editbtn.className='btn';
-editbtn.appendChild(document.createTextNode('edit'));
-li.appendChild(editbtn);
-
-itemList.appendChild(li);
-    
     }
+    document.addEventListener('click',edititem);
 
-
-    itemList.addEventListener('click',deleteitem);
-    itemList.addEventListener('click',edititem);
-
-function deleteitem(e){
-    if(e.target==deletbtn){
-        if(confirm('r u sure?')){
+    function edititem(e){
+        if(e.target==edibtn){
             var li=e.target.parentElement;
-            localStorage.removeItem(emaili.value);
-            itemList.removeChild(li);
+            for(var i=0;i<itemlist.getElementsByClassName('item').length;i++){
+                if(itemlist.getElementsByClassName('item')[i]==li){
+                    let myobj_deserialized1=JSON.parse(itemlist.getElementsByClassName('item')[i].firstChild.textContent);
+                    Form.querySelector('#expence').value=myobj_deserialized1.expence
+                    Form.querySelector('#description').value=myobj_deserialized1.description;
+                    Form.querySelector('#category').value=myobj_deserialized1.category;
+                    localStorage.removeItem(Obj.description);
+
+                }
+            }
+            itemlist.removeChild(li)
         }
     }
-}
-function edititem(e){
-    if(e.target==editbtn){
-    var li=e.target.parentElement;
-for(var i=0;i<itemList.getElementsByClassName('items').length;i++)
-{
-    if(itemList.getElementsByClassName('items')[i]==li){
-        
-        let myobj_deserialized1=JSON.parse(itemList.getElementsByClassName('items')[i].firstChild.textContent)
-        
-        myForm.querySelector('#name').value=myobj_deserialized1.name;
-        myForm.querySelector('#email').value=myobj_deserialized1.email;
-    }
-}
-        localStorage.removeItem(emaili.value);
-        itemList.removeChild(li);
-    }
-}
+}  
+
 }
